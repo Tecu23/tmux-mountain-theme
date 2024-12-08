1361ec09d9b783c4fce18d2adf826be0b04d851a
Tecu23 <andrei.teculescu@gmail.com>
2024-12-08 18:51:52 +0200

    update the module and window formatting

--- src/theme.sh
+++ src/theme.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# TODO: Make transparent background setting work
-# TODO: Add reverse icon logic
+# TODO: Add window number position logic
+# TODO: Add option to override the window colors
+# TODO: Add hex color support
+# TODO: Update README will all these options
 
 # setting the locale, some users have issues with different locales, this enforces the correct one
 export LC_ALL=en_US.UTF-8
@@ -23,26 +25,36 @@ main() {
     border_style_inactive_pane=$(get_tmux_option "@mountain_theme_inactive_pane_border_style" "${PALLETE[bright_black]}")
 
     session_icon=$(get_tmux_option "@mountain_theme_session_icon" "")
-    active_window_icon=$(get_tmux_option "@mountain_theme_plugin_active_window_icon" " ")
-    inactive_window_icon=$(get_tmux_option "@mountain_theme_plugin_inactive_window_icon" " ")
-    zoomed_window_icon=$(get_tmux_option "@mountain_theme_plugin_zoomed_window_icon" " ")
-    pane_synchronized_icon=$(get_tmux_option "@mountain_theme_plugin_pane_synchronized_icon" "✵")
+
+    icon_window_bell=$(get_tmux_option "@mountain_theme_bell_window_icon" "󰂞 ")
+    icon_window_mark=$(get_tmux_option "@mountain_theme_mark_window_icon" "󰃀 ")
+    icon_window_silent=$(get_tmux_option "@mountain_theme_silent_window_icon" "󰂛 ")
+    icon_window_activity=$(get_tmux_option "@mountain_theme_activity_window_incon" "󰖲 ")
+    icon_window_current=$(get_tmux_option "@mountain_theme_current_window_icon" "󰖯 ")
+    icon_window_last=$(get_tmux_option "@mountain_theme_last_window_icon" "󰖰 ")
 
     active_window_title=$(get_tmux_option "@mountain_theme_active_window_title" "#W ")
+    active_window_number=$(get_tmux_option "@mountain_theme_active_window_number" "#I")
     inactive_window_title=$(get_tmux_option "@mountain_theme_inactive_window_title" "#W ")
+    inactive_window_number=$(get_tmux_option "@mountain_theme_active_window_number" "#I")
 
-    left_separator=$(get_tmux_option "@mountain_theme_left_separator" ' ')    # '' ""
-    middle_separator=$(get_tmux_option "@mountain_theme_middle_separator" '█') #
-    right_separator=$(get_tmux_option "@mountain_theme_right_separator" "")   # ''thee
+    window_left_separator=$(get_tmux_option "@mountain_theme_window_left_separator" '█')
+    window_middle_separator=$(get_tmux_option "@mountain_theme_window_middle_separator" "█")
+    window_right_separator=$(get_tmux_option "@mountain_theme_window_right_separator" '█ ')
 
-    IFS=',' read -ra status_options <<<"$(get_tmux_option "@mountain_theme_status_options", "datetime,application,user")"
+    window_fill=$(get_tmux_option "@mountain_theme_window_fill" "number")
+    window_status_enable=$(get_tmux_option "@mountain_theme_window_status_enable" "true")           # show window status options
+    window_status_icon_enable=$(get_tmux_option "@mountain_theme_window_status_icon_enable" "true") # override the current window icons
 
-    if [ "$transparent" = "true" ]; then
-        right_separator_inverse=$(get_tmux_option "@mountain_theme_transparent_right_separator_inverse" " ") # ''
-    fi
+    status_fill=$(get_tmux_option "@mountain_theme_status_fill" "icon")
+    status_left_separator=$(get_tmux_option "@mountain_theme_status_left_separator" ' ')
+    status_right_separator=$(get_tmux_option "@mountain_theme_status_right_separator" "")
+    status_right_separator_inverse=$(get_tmux_option "@mountain_theme_right_separator_inverse" "true")
 
-    tmux set-option -g status-left-length 100
-    tmux set-option -g status-right-length 100
+    IFS=',' read -a status_modules <<<$(get_tmux_option "@mountain_theme_status_modules" "datetime,application,user")
+
+    tmux set-option -gq status-left-length "100"
+    tmux set-option -gq status-right-length "100"
 
     # messages
     tmux set-option -gq message-style "fg=${PALLETE[fg]},bg=default,align=centre"
@@ -60,43 +72,42 @@ main() {
     tmux set-option -g pane-border-style "#{?pane_synchronized,fg=$border_style_active_pane,fg=$border_style_inactive_pane}"
 
     ### Create Session String
-    tmux set-option -g status-left "$(generate_session_string "$session_icon" "$left_separator" "$right_separator" "$transparent" "${PALLETE[bright_red]}" "${PALLETE[bright_green]}" "${PALLETE[normal_black]}")"
+    tmux set-option -g status-left "$(generate_session_string "$session_icon" "$window_left_separator" "$window_right_separator" "$transparent" "${PALLETE[bright_red]}" "${PALLETE[bright_green]}" "${PALLETE[normal_black]}")"
 
     ### Create Active Window Pane
-    tmux set-window-option -g window-status-current-format "$(generate_window_string "$active_window_icon" "$zoomed_window_icon" "$pane_synchronized_icon" "$left_separator" "$middle_separator" "$right_separator" "$transparent" "$active_window_title" "${PALLETE[bright_cyan]}" "${PALLETE[normal_cyan]}" "${PALLETE[normal_black]}" "${PALLETE[bg]}")"
+    tmux set-window-option -g window-status-current-format "$(generate_window "${active_window_number}" "${PALLETE[bright_cyan]}" "${PALLETE[normal_black]}" "${PALLETE[bright_cyan]}" "${active_window_title}" "${window_fill}")"
 
     ### Create Inactive Window Pane
-    tmux set-window-option -g window-status-format "$(generate_window_string "$inactive_window_icon" "$zoomed_window_icon" "$pane_synchronized_icon" "$left_separator" "$middle_separator" "$right_separator" "$transparent" "$inactive_window_title" "${PALLETE[bright_black]}" "${PALLETE[bright_white]}" "${PALLETE[normal_black]}" "${PALLETE[bg]}")"
+    tmux set-window-option -g window-status-format "$(generate_window "${inactive_window_number}" "${PALLETE[bright_black]}" "${PALLETE[normal_black]}" "${PALLETE[normal_white]}" ${inactive_window_title} "${window_fill}")"
 
     ### Right side
     tmux set-option -g status-right ""
 
-    for option in "${status_options[@]}"; do
-
-        if [ ! -f "${current_dir}/status/${option}.sh" ]; then
+    for module in "${status_modules[@]}"; do
+        if [ ! -f "${current_dir}/status/${module}.sh" ]; then
             # TODO: Update logic to handle user created statuses
-            # if no option is available, then do nothing for now
+            # if no module is available, then do nothing for now
             #
-            # tmux set-option -ga status-right "${option}"
-            echo "${option}"
+            # tmux set-option -ga status-right "${module}"
+            echo "${module}"
         else
 
-            status_script_path="${current_dir}/status/${option}.sh"
+            status_script_path="${current_dir}/status/${module}.sh"
 
             # shellcheck source=src/status/user.sh
             . "$status_script_path"
 
-            icon_var="status_${option}_icon"
-            accent_color_var="status_${option}_accent_color"
-            icon_color_var="status_${option}_icon_color"
-            text_var="status_${option}_text"
+            icon_var="status_${module}_icon"
+            accent_color_var="status_${module}_accent_color"
+            icon_color_var="status_${module}_icon_color"
+            text_var="status_${module}_text"
 
             status_icon="${!icon_var}"
             accent_color="${PALLETE["${!accent_color_var}"]}"
             icon_color="${PALLETE["${!icon_color_var}"]}"
             status_text="${!text_var}"
 
-            tmux set-option -ga status-right "$(generate_module_string "$status_icon" "$status_text" "$accent_color" "$icon_color" "$left_separator" "$middle_separator" "$right_separator" "${PALLETE[normal_black]}")"
+            tmux set-option -ga status-right "$(generate_status_module "$module" "$status_icon" "${accent_color}" "$status_text")"
         fi
     done
     tmux set-window-option -g window-status-separator ''
--- src/utils.sh
+++ src/utils.sh
@@ -38,84 +38,143 @@ generate_session_string() {
     local right_separator="$3"
     local transparent="$4"
 
-    separator_start="#[bg=default]#{?client_prefix,#[fg=$5],#[fg=$6]}${left_separator:?}"
+    separator_start="#[bg=default]#{?client_prefix,#[fg=$5],#[fg=$6]}${window_left_separator:?}"
     text="#{?client_prefix,#[bg=$5],#[bg=$6]}#[fg=$7,bold]${session_icon} #S"
-    separator_end="#[bg=default]#{?client_prefix,#[fg=$5],#[fg=$6]}${right_separator:?}#[none]"
+    separator_end="#[bg=default]#{?client_prefix,#[fg=$5],#[fg=$6]}${window_right_separator:?}#[none]"
 
     echo "${separator_start}${text} ${separator_end}"
 }
 
-# Function: generate_window_string()
+# Function: generate_status_module()
 # Description:
-#   creates the window string (either active / inactive) string for the tmux status bar
+#   creates the module string
 #
 # Parameters:
-#   $1  (string) - window icon
-#   $2  (string) - zoomed window icon
-#   $3  (string) - synchronized pane icon
-#   $4  (string) - the left separator
-#   $5  (string) - the middle separator
-#   $6  (string) - the right separator
-#   $7  (string) - whether the background is transparent or not
-#   $8  (string) - the window text formatting
-#   $9  (string) - number background color
-#   $10 (string) - text background color
-#   $11 (string) - text foreground color
-#   $12 (string) - background color (when non-transparent)
+#   $1 (string) - the index of the module
+#   $2 (string) - provided icon
+#   $3 (string) - provided color
+#   $4 (string) - provided text
+#
+# Output:
+#   - return: the status module string
+generate_status_module() {
+    local index=$1
+    local icon=$2
+    local color=$3
+    local text=$4
+
+    local background=${PALLETE[bg]}
+    if [ "$transparent" = "true" ]; then
+        local background="default"
+    fi
+
+    case "$status_fill" in
+    "icon")
+        local show_icon="#[fg=${PALLETE[normal_black]},bg=$color,nobold,nounderscore,noitalics]$icon "
+        local show_text="#[fg=${color},bg=${PALLETE[normal_black]}] $text"
+
+        local show_left_separator="#[fg=$color,bg=$background,nobold,nounderscore,noitalics]$status_left_separator"
+        local show_right_separator="#[fg=${PALLETE[normal_black]},bg=$background]$status_right_separator"
+        ;;
+    "all")
+        local show_icon="#[fg=${PALLETE[normal_black]},bg=$color,nobold,nounderscore,noitalics]$icon "
+        local show_text="#[fg=${PALLETE[normal_black]},bg=${color}] $text"
+
+        local show_left_separator="#[fg=$color,bg=$background,nobold,nounderscore,noitalics]$status_left_separator"
+        local show_right_separator="#[fg=$color,bg=$background]$right_separator"
+        ;;
+    *) ;;
+    esac
+
+    # NOTE: Disable inverse separator option for now
+    # TODO: Fix this
+    status_right_separator_inverse="false"
+    if [ "$status_right_separator_inverse" = "true" ]; then
+        local show_right_separator="#[fg=$background,bg=${color}]$status_left_separator"
+    fi
+
+    echo "$show_left_separator$show_icon$show_text$show_right_separator"
+}
+
+# Function: generate_window_icon()
+# Description:
+#   overrides the default icons
+#
+# Parameters:
+#   -
+#
+# Output:
+#   - return: the updated icons string
+generate_window_icon() {
+    if [ "$window_status_icon_enable" = "true" ]; then
+        local show_window_status="#{?window_activity_flag,${icon_window_activity},}#{?window_bell_flag,${icon_window_bell},}#{?window_silence_flag,${icon_window_silent},}#{?window_active,${icon_window_current},}#{?window_last_flag,${icon_window_last},}#{?window_marked_flag,${icon_window_mark},}#{?window_zoomed_flag,${icon_window_zoom},} "
+    else
+        local show_window_status="#F"
+    fi
+
+    echo "$show_window_status"
+}
+
+# Function: generate_window()
+# Description:
+#   creates the window string
+#
+# Parameters:
+#   $1 (string) - the formatted number string
+#   $2 (string) - provided number color
+#   $3 (string) - provided background text color
+#   $4 (string) - provided foreground text color
+#   $5 (string) - provided text
+#   $6 (string) - how much to fill: "none", "number", "all"
 #
 # Output:
 #   - return: the window string
-generate_window_string() {
-    local left_separator=$4
-    local middle_separator=$5
-    local right_separator=$6
-    local transparent=$7
-    local active_window_title=$8
+generate_window() {
+    local number=$1
+    local color=$2
+    local color_bg=$3
+    local color_fg=$4
+    local text=$5
+    local fill=$6
 
-    separator_start="#[bg=default,fg=$9]${left_separator}#[none]"
-    number="#[bg=$9]#[fg=${11}]#I "
-    separator_middle="#[bg=${11},fg=${11}]${middle_separator}#[none]"
-    separator_end="#[bg=default,fg=${11}]${right_separator}#[none]"
+    local background=${PALLETE[bg]}
+    if [ "$transparent" = "true" ]; then
+        local background="default"
+    fi
 
-    echo "${separator_start}${number}${separator_middle}#[fg=${10}]#{?window_zoomed_flag,$2,$1}${active_window_title}#{?pane_synchronized,$3,}${separator_end}#[none]"
-}
+    if [ "$window_status_enable" = "true" ]; then
+        local icon="$(generate_window_icon)"
+        text="$text $icon"
+    fi
 
-# Function: generate_module_string()
-# Description:
-#   creates a module string for a selected module
-#
-# Parameters:
-#   $1  (string) - module icon
-#   $2  (string) - module text
-#   $3  (string) - module accent color
-#   $4  (string) - module icon background color
-#   $5  (string) - the left separator
-#   $6  (string) - the middle separator
-#   $7  (string) - the right separator
-#   $8  (string) - background color
-#
-# Output:
-#   - return: the module string
-generate_module_string() {
-    local status_icon=$1
-    local status_text=$2
-    local accent_color=$3
-    local icon_color=$4
-    local bg_color=$8
+    case "$fill" in
+    "none")
+        local show_number="#[fg=${PALLETE[fg]},bg=${PALLETE[bright_black]}]$number "
+        local show_middle_separator="#[fg=${PALLETE[normal_black]},bg=${PALLETE[bright_black]},nobold,nounderscore,noitalics]$window_middle_separator"
+        local show_text="#[fg=${PALLETE[fg]},bg=${PALLETE[normal_black]}]$text"
 
-    local left_separator=$5
-    local middle_separator=$6
-    local right_separator=$7
+        local show_left_separator="#[fg=${PALLETE[bright_black]},bg=$background,nobold,nounderscore,noitalics]$window_left_separator"
+        local show_right_separator="#[fg=${PALLETE[normal_black]},bg=$background]$window_right_separator"
+        ;;
+    "number")
+        local show_number="#[fg=${color_bg},bg=$color]$number "
+        local show_middle_separator="#[fg=${color_bg},bg=${color},nobold,nounderscore,noitalics]$window_middle_separator"
+        local show_text="#[fg=${color_fg},bg=${color_bg}]$text"
 
-    local string=""
+        local show_left_separator="#[fg=$color,bg=$background,nobold,nounderscore,noitalics]$window_left_separator"
+        local show_right_separator="#[fg=${color_bg},bg=$background]$window_right_separator"
+        ;;
+    "all")
+        local show_number="#[fg=${color_bg},bg=$color]$number "
+        local show_middle_separator="#[fg=${color},bg=${color},nobold,nounderscore,noitalics]$window_middle_separator"
+        local show_text="#[fg=${color_bg},bg=${color}]$text"
 
-    local separator_icon_start="#[fg=${icon_color},bg=default]${left_separator}#[none]"
-    local separator_icon_end="#[fg=${bg_color},bg=${icon_color}] ${middle_separator}#[none]"
-    local separator_end="#[fg=${bg_color},bg=default]${right_separator}#[none]"
+        local show_left_separator="#[fg=$color,bg=$background,nobold,nounderscore,noitalics]$window_left_separator"
+        local show_right_separator="#[fg=${color},bg=$background]$window_right_separator"
+        ;;
+    *) ;;
+    esac
 
-    string=${string}"${separator_icon_start}#[fg=${bg_color},bg=${icon_color}]${status_icon}${separator_icon_end}"
-    string=${string}"#[fg=${accent_color},bg=${bg_color}]${status_text}#[none]"
-    string=${string}${separator_end}
+    echo "$show_left_separator$show_number$show_middle_separator$show_text$show_right_separator"
 
-    echo "$string"
 }
